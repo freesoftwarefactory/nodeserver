0628e2abf2806be6bc47dcbb9747473e156784c8
Christian Salazar <christiansalazarh@gmail.com>
2019-06-01 08:43:42 -0400

    quick server

---
+++ README.md
@@ -0,0 +1,165 @@
+# Local Server
+
+Serve your local lambda functions via fully configurable http endpoints.
+	
+author:  Cristian Salazar <christiansalazarh@gmail.com>
+
+## Sample Application
+
+First, put this module into your "./node_modules" directory, name it: "server".
+
+
+Next, create a "main.js" file, which contains:
+
+```
+var fs = require("fs");
+var routes = JSON.parse(fs.readFileSync("./routes.json"));
+var server = require("server/init.js");
+
+server(routes);
+```
+
+Customize by modifying the routes file and provide "Actions", in this form:
+
+File: ./actions/SomeAction.js (look for: "SomeAction" in your routes.js)
+```
+exports.handler = (event, context, callback) => {
+    console.log('hello from home Action, context=',context,'event',event);    
+
+    // you returns a JSON encoded response because your action is defined
+    // in this way in the routes.json file (see "content-type")
+    callback(null, { "something" : "hello world" }); 
+}
+```
+
+
+## Sample Routes (routes.json)
+
+A sample file is provided: 
+
+	local-server.json 	
+
+```
+{
+	 "port" : 8100
+	,"routes" : {
+
+        "/" : {
+            "vars" : [],
+            "lambda" : { "name" : "SomeAction", "exports" : "handler" },
+            "GET" : {
+                "event" : {
+                    "some" : "thing" 
+                },
+                "context" : {
+                    
+                },
+                "responseHeaders" : {
+					"Access-Control-Allow-Origin" : "http://coquito.local",
+					"content-type" : "application/json"		
+                }
+            }
+        },
+
+		"\/property\/([0-9]+)" : {
+			"vars"	 : ["{sysid}"],
+			"lambda" : { "name" : "RetsApiGetProperty" , "exports" : "handler" },
+			"GET" : {
+				"event"  : { 
+					"development" : true,
+					"params" : { "path" : { "sysid" : "{sysid}" } }					
+				},
+				"context" : {
+					
+				},
+				"responseHeaders" : {
+					"Access-Control-Allow-Origin" : "http://coquito.local",
+					"content-type" : "application/json"		
+				}
+			}
+		}
+	}
+}
+```
+
+This file define your endpoints. This example provides you with a endpoint:
+
+	http://localhost:8100/property/123456
+
+which will execute the local lambda code at: 
+
+	"/your/project/lambda/RetsApiGetProperty/src"
+
+the result from your lambda function will be echoed to the browser as
+a "application/json", and provides some basic CORS headers too (as an example)
+
+## HTTPS
+
+You can server HTTPS endpoint by adding this attributes to your setup in
+local-server.json:
+
+```
+	,"port" : 8443
+	,"secure" : true
+	,"privatekey" : "/your/cert/some.key"
+	,"certificate" : "/your/cert/some.crt"
+```
+
+So, in consecuence you will have a https endpoint:
+
+	https://localhost:8443/
+
+## Processing Paths and Reading Variables from a Path:
+
+Lets suppose you provide some value via path, something like this:
+
+	http://localhost:8100/property/123/ABC
+
+In order to get the provided values ("123" and "ABC") from inside a lambda
+function you must provide a setup via file 'lambda-server.json',
+using a regular expression similar to this one in your route definition:
+
+```
+"routes" : {
+	"\/property\/([0-9]+)/([a-z]+)" : { ... },
+}, ... 
+```
+
+The next step is to provide the var names definition in the 'vars' attribute:
+
+```
+...
+"\/property\/([0-9]+)/([a-z]+)" : {
+	"vars"	 : ["{sysid}","{other}"],
+	...
+}
+```
+
+Take a look at the sample rule definition (see: Configure) it provides
+a setup for the 'events' object, this object will be passed to your lambda
+function and can receive the values in this way:
+
+```
+"event"  : { 
+	"development" : true,
+	"params" : { "path" : { "sysid" : "{sysid}" } }					
+},
+```
+
+Note the "event" object and The variable "{sysid}", which has been parsed
+and now it has the value '123' taken from the path. This value is passed
+to your 'event' object, so in your lambda function you can read it by using
+a call similar to this one:
+
+	console.log(event.params.path.sysid); // output: 123
+
+## Variable Names:
+
+The variables taken from the path must follow this rule:
+
+	{[a-z0-9\s\.\_\-]+}
+
+example:
+
+	{abc} {123} {abc123} {abc-123} {abc 123}
+
---
+++ init.js
@@ -0,0 +1,152 @@
+/**
+	Provides a simple api gateway interface.
+
+	author:  Cristian Salazar <christiansalazarh@gmail.com>
+*/
+var fs = require("fs");
+var url = require("url");
+// var def = JSON.parse(fs.readFileSync("../routes.json"));
+var currentRoutes = {};
+
+/**
+	find a route and parse path variables. (see "Processing Paths" on README.md)
+*/
+var findRoute = function(routes, path){
+	for(var pathExpression in routes){
+		if (routes.hasOwnProperty(pathExpression)) {
+			var m="";
+			if(m = path.match(new RegExp(pathExpression,"i"))){
+				var route = routes[pathExpression];
+				var values = {};
+				route.vars.forEach(function(varname,i){
+					Object.defineProperty( values , varname , 
+					        { value: m[i+1], writable: true, 
+								enumerable: true, configurable: true });
+				});
+				route.values = values;
+				return route;
+			}
+		}
+	}
+	return null;
+};
+
+/**
+	parse values taken into a given object:
+
+	{ 
+		"development" : true,
+		"params" : { "path" : { "sysid" : "{sysid}" } }					
+	}
+
+	values:
+
+	{ "{sysid}" : "123" , "{other}" : "ABC" }
+*/
+var replaceVariables = function(object, values){
+	return JSON.parse(JSON.stringify(object).replace(
+			/{[a-z\s0-9\.\-\_]+}/gi,function(fullvar, nameOnly){
+			return values[fullvar] ? values[fullvar] : "fullvar";
+	}));
+};
+
+/**
+	execute the route depending on the "request.method". 
+*/
+var executeRoute = function(route, request, response){
+	var methodInfo = route[request.method];
+	if((null == methodInfo) || (undefined==methodInfo)){
+		// method not supported
+		var msg = "[method "+request.method
+			+" not supported in your route definition]";
+		return new Promise((resolve,reject) => {
+			reject({ route: route, request: request, response: response , msg: msg});
+		});
+	}	
+	var event = replaceVariables(methodInfo.event, route.values);
+	event.body = null;
+	var context = methodInfo.context;
+	// execute the lambda function:
+	var path = process.env.PWD+"/actions/"+route.lambda.name+".js";
+	console.log("execute: "+route.lambda.name,"("+path+")");
+	// this environment var can be used inside lambda functions
+	process.env.LAMBDA_DIR=path;
+	var lambda = require(path);
+	return new Promise((resolve,reject) => {
+		var doJob = function(){
+			// TODO: use route.lambda.handler insted of hardwired "handler()"
+			lambda.handler(event,context,function(err,data){
+				if(err){
+					reject(err);	
+				}else{
+					resolve({ route: route, event: event, 
+						method : methodInfo, payload: data,
+							request: request, response: response });	
+				}
+			});
+		};
+		if('POST'==request.method){
+			var chunks = [];
+			request.on('data',chunk => chunks.push(chunk));
+			request.on('end',()=>{
+				event.body = Buffer.concat(chunks);
+				doJob();
+			});
+		}else
+		doJob();
+	});
+};
+
+var handler = function(request, response) {
+	var path = url.parse(request.url).pathname;
+	try{
+	var route = findRoute(currentRoutes.routes,path);	
+	if(route){
+		try{
+			console.log("values:",route.values);
+			console.log("headers:",request.headers);
+			executeRoute(route, request, response).then(function(r){
+				r.response.writeHead(200, r.method.responseHeaders);
+				r.response.write(
+					("string" == (typeof r.payload)) ? r.payload 
+						: JSON.stringify(r.payload));
+				r.response.end();
+			}).catch(function(r){
+				console.log("executeRoute Promise.catch:",r.msg);
+				r.response.end();
+			});	
+		}catch(ex){
+			console.log("exception at route level:",ex,"route:",route);
+			response.end();	
+		}					
+	}else{
+		console.log("invalid path",path);
+		response.end();
+	}}catch(ex){
+		console.log("exception (path="+path+")",ex);	
+		response.end();
+	}
+};
+
+module.exports = function(routes)
+{
+    currentRoutes = routes;
+
+    process.on('uncaughtException', function(err) {
+        console.log("(unhandled exception)",err)
+    });
+
+    /*entry point*/
+    if(true == routes.secure){
+        var proto = require("https");
+        var key = fs.readFileSync(routes.privatekey).toString();
+        var cert = fs.readFileSync(routes.certificate).toString();
+        console.log("https://localhost:"+routes.port+" (is now active) using:\n"
+            +"cert:"+routes.certificate+"\nkey:"+routes.privatekey+"\n");
+        proto.createServer({key:key,cert:cert},handler).listen(routes.port);
+    }else{
+        var proto = require("http");
+        console.log("http://localhost:"+routes.port+" (is now active)");	
+        proto.createServer(handler).listen(routes.port);
+    }
+};
